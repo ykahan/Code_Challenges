4f8f58806c1440b4a4807f553e6aafa4dda80bea
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2020-06-21 23:27:18 +0300

    Got rid of Map, added second Set to replace it

--- src/CodeWars/BraceChecker.java
+++ src/CodeWars/BraceChecker.java
@@ -99,5 +99,7 @@ public class BraceChecker {
         System.out.println(isValid(str));
         str = "[(])";
         System.out.println(isValid(str));
+        str = ")(";
+        System.out.println(isValid(str));
     }
 }
